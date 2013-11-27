25457e1409584efdd12abc5e40c5d713468c2052
Jain Johny <jain.johny@flipkart.com>
2013-11-28 01:14:08 +0530

    unit tests for server modules, config file syntax change, bug fixes

---
+++ src/server/usr/local/lib/site_perl/HostDB/Auth/File.pm
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+package HostDB::Auth::File;
+
+use strict;
+use HostDB::Shared qw( &get_conf $logger );
+use Data::Dumper;
+use YAML::Syck;
+use NetAddr::IP;
+
+# IMP: Do not confess/cluck in this module. Call trace will contain the password.
+my $creds;
+
+sub exists {
+    my ($user) = @_;
+    _get_creds();
+    return exists $creds->{$user};
+}
+
+sub auth {
+    my ($user, $pass) = @_;
+    _get_creds();
+    return 0 if ! exists $creds->{$user};
+    my $md5sum = `echo "$pass" | /usr/bin/md5sum - | cut -d' ' -f1`;
+    chomp $md5sum;
+    return ($md5sum eq $creds->{$user}->{password}) ? 1 : 0;
+}
+
+sub allowed {
+    my ($user, $ip) = @_;
+    _get_creds();
+    return 0 if ! exists $creds->{$user};
+    my $myip = NetAddr::IP->new($ip);
+    foreach (@{$creds->{$user}->{allow}}) {
+        my $range = NetAddr::IP->new($_);
+        return 1 if $range->contains($myip);
+    }
+    return 0;
+}
+
+sub _get_creds {
+    return if $creds;
+    my $creds_file = get_conf('auth.file.creds_file') or $logger->logdie("5003: Config auth.file.creds_file not set");
+    $creds = LoadFile($creds_file) or $logger->logdie("5001: Unable to load YAML in $creds_file");
+}
+
+1;
---
+++ src/server/usr/local/lib/site_perl/HostDB/Auth/LDAP.pm
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+package HostDB::Auth::LDAP;
+
+use strict;
+use HostDB::Shared qw( &get_conf $logger );
+use Data::Dumper;
+use Net::LDAP;
+
+sub auth {
+    # IMP: Do not confess/cluck in this function. Call trace will contain the password.
+    my ($user, $pass) = @_;
+    $user or $logger->logcroak("4001: Missing parameter - username");
+    my $server = get_conf('auth.ldaps.server') or $logger->logdie("5003: Config auth.ldaps.server not set");
+    my $port = get_conf('auth.ldaps.port') || 636;
+    my $users_dn = get_conf('auth.ldaps.users_dn') or $logger->logdie("5003: Config auth.ldaps.users_dn not set");
+    my $user_dn = "uid=$user,$users_dn";
+    my $ldap = Net::LDAP->new("ldaps://$server", port => $port) or $logger->logdie("5030: Could not create LDAP object because:\n$!");
+    my $msg = $ldap->bind($user_dn, password => $pass);
+    #my $ldapSearch = $ldap->search(base => $LDAP_BASE, filter => "uid=$user");
+    return ($msg->is_error) ? 0 : 1;
+}
+
+sub groups {
+    my $user = shift or $logger->logcroak("4001: Missing parameter - username");
+    my @groups = map { chomp; $_ } `/usr/bin/getent group | grep "\[^a-z\]$user\[^a-z\]" | cut -d: -f1`; # improve
+    return @groups;
+}
+
+1;
